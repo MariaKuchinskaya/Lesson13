27ba8d6669fe51ee710b22cf05c04b333b5a397f
Maria Kuchinskaya <mariakuchinskaya16@gmail.com>
2021-12-01 21:32:11 +0300

    Lesson13

---
+++ Lesson13/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Lesson13
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+
+        public string ShopName { get; set; }
+
+        public Product(string productName, string shopName)
+        {
+            ProductName = productName;
+            ShopName = shopName;
+        }
+    }
+}
---
+++ Lesson13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lesson13
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                var products = new Product[5];
+                var product1 = new Product("Cloth", "H&M");
+                var product2 = new Product("Orange", "Green");
+                var product3 = new Product("Skirt", "Gucci");
+                var product4 = new Product("Milk", "Green");
+                var product5 = new Product("Dress", "H&M");
+                products[0] = product1;
+                products[1] = product2;
+                products[2] = product3;
+                products[3] = product4;
+                products[4] = product5;
+
+
+                Array.Sort(products, (x, y) => string.Compare(x.ProductName, y.ProductName));
+                var result = Array.Find(products, x => x.ProductName.Equals("Orange"));
+                if (result == null)
+                {
+                    throw new ShopNotFoundException();
+                }
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
